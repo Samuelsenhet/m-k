9d2cd6c6f8c68b60009dd634f2b30b8e3d8ba218
Samuelsenhet <connection.dts@gmail.com>
2026-03-02 22:31:55 +0100

    commit to main

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'se.maakapp.maak',
+  appId: 'com.samuelsenhet.maak',
   appName: 'MÄÄK',
   webDir: 'dist',
   plugins: {
