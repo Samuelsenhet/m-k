c94d8a5c3ddb9a3daa3c2b0620693906fc494186
Samuelsenhet <connection.dts@gmail.com>
2026-03-04 20:33:12 +0100

    fix(eas): upgrade node to 22.12.0 and sync lock file

---
+++ ios/App/CapApp-SPM/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  CapApp-SPM
+//
+//  Created by Uncsenhet on 2026-03-03.
+//
+
+import Foundation
---
+++ ios/App/CapApp-SPM/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CapApp-SPM
+//
+//  Created by Uncsenhet on 2026-03-03.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
